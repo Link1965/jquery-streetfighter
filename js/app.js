e7cd3bc79c62cfe12655fc8ecff63bc3d2803b63
git config --global user.name "Link1965 <git config --global user.email nicci.hydak@gmail.com>
2016-02-08 18:27:54 -0500

    Final version

--- js/app.js
+++ js/app.js
@@ -1,34 +1,55 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
-  	$('.ryu-ready').show();
+    $('.ryu-ready').show();
+    $('.ryu-cool').hide();
   })
- .mouseleave(function() {
+
+  .mouseleave(function() {
     $('.ryu-ready').hide();
-	$('.ryu-still').show();
+    $('.ryu-still').show();
   })
+
   .mousedown(function() {
-  	playHadouken();
+    playHadouken();
     $('.ryu-still').hide();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
-  		{'left': '1020px'},
-  		500,
-  		function() {
-  		$(this).hide();
-   		$(this).css('left', '565px');
-
-	});
+      {'left': '1020px'},
+      500,
+      function() {
+      $(this).hide();
+      $(this).css('left', '565px');
+    });
   })
+
   .mouseup(function() {
     $('.ryu-still').show();
     $('.ryu-ready').hide();
     $('.ryu-throwing').hide();
-    $('.hadouken').hide();
   });
-  });
+
+  $(document).keydown(function(event) {
+    if (event.which == 88) {
+      $('.ryu-cool').show();
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+    }
+  })
+
+  $(document).keyup(function(event) {
+    if (event.which == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+      $('.ryu-ready').hide();
+    }
+  })
+
+  
+});
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
